5854f8c041ec67b3aaed496e18401cc73a2b2205
Neskazu <erikfil1995@gmail.com>
2024-06-26 16:35:07 +0600

    ProjectController, TaskContoller
    Added 2 main controllers for tasks and projects

--- ProjectManagment/Controllers/ProjectController.cs
+++ ProjectManagment/Controllers/ProjectController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProjectManagment.Data;
+using ProjectManagment.Models;
 
 namespace ProjectManagment.Controllers
 {
@@ -17,5 +18,96 @@ namespace ProjectManagment.Controllers
             var projects = await context.Projects.Include(p=>p.Owner).ToListAsync();
             return View(projects);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Name,Description")] ProjectModel project)
+        {
+            if (ModelState.IsValid)
+            {
+                project.OwnerId = 1/* current user id */;
+                context.Add(project);
+                await context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(project);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var project = await context.Projects.FindAsync(id);
+            if(project == null) 
+            {
+                return NotFound();
+            }
+            return View(project);  
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] ProjectModel project)
+        {
+            if(id!=project.Id) 
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid) 
+            {
+                try 
+                {
+                    context.Update(project);
+                    await context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException) 
+                {
+                    if (!ProjectExists(project.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(project);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var project = await context.Projects
+                .Include(p => p.Owner)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return View(project);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var project = await context.Projects.FindAsync(id);
+            context.Projects.Remove(project);
+            await context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProjectExists(int id)
+        {
+            return context.Projects.Any(e => e.Id == id);
+        }
     }
+
 }
---
+++ ProjectManagment/Controllers/TaskController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectManagment.Data;
+using ProjectManagment.Models;
+
+namespace ProjectManagment.Controllers
+{
+    
+    public class TaskController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public TaskController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //task details
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) 
+            {
+                return NotFound();
+            }
+            var task = await context.Tasks
+                .Include(t => t.Project)
+                .Include(t => t.AssignedUser)
+                .FirstOrDefaultAsync(t => t.Id == id);
+            if (task == null) 
+            {
+                return NotFound();
+            }
+            return View(task);
+        }
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Title,Description,Deadline,ProjectId,AssignedUserId")] TaskModel task)
+        {
+            if (ModelState.IsValid) 
+            {
+                task.Status = Models.TaskStatus.New;
+                context.Add(task);
+                await context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(task);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null) 
+            {
+                return NotFound();
+            }
+            var task = await context.Tasks.FindAsync(id);
+            if(task == null) 
+            {
+                return NotFound();
+            }
+            return View(task);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Desctiption,Deadline,Status,AssingnedUserId")] TaskModel task)
+        {
+            if (id != task.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    context.Update(task);
+                    await context.SaveChangesAsync();
+
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TaskExist(task.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(task);
+        }
+
+        private bool TaskExist(int id)
+        {
+            return context.Tasks.Any(t => t.Id == id);
+        }
+    }
+}
